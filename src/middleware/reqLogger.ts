ae0deb0c491d18c333953cba9bbca60f7f0ea5d8
Nathami-n <georgenathan010@gmail.com>
2024-06-12 23:59:59 +0300

    Add test

--- src/middleware/reqLogger.ts
+++ src/middleware/reqLogger.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 
 import {logger} from '../utils/functions';
+import { CustomRequest } from "./jwt";
 
-export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
-    logger(`${req.method}\t ${req.url}\t ${req.headers.origin}`, "reqlog.txt");
+export const requestLogger = (req: CustomRequest, res: Response, next: NextFunction) => {
+    logger(`${req.method}\t ${req.url}\t ${req.headers.origin} \t ${req.user_email}`, "reqlog.txt");
     next();
 };
